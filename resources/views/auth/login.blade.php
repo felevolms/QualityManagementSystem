06e7a67737b14319d598e509187c28345d54fa63
Marek <marektomczak5503@icloud.com>
2020-04-23 17:38:42 +0200

    Google Auth Button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+                                <a class="btn btn-primary" href="{{ url('login/google') }}" role="button">
+                                    {{ __('Login with Google') }}
+                                </a>
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
