a0cfb7ec47d88a89acd28d844f4330cb961b7088
lajtof <thel4jtowy@gmail.com>
2020-05-26 16:21:52 +0200

    scss

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,9 +10,7 @@
                     <div class="card-body">
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-
                                 <a href="{{ url('/login/google') }}" class="btn btn-primary">Zaloguj z Google</a>
-
                             </div>
                         </div>
                     </div>
